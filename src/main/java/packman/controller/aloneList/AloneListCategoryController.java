42423af8b7f60baa0eb000e70870ef5d25668630
dingding-21 <phj8221@naver.com>
2023-01-17 00:00:36 +0900

    feat: controller 추가

---
+++ src/main/java/packman/controller/aloneList/AloneListCategoryController.java
@@ -0,0 +1,11 @@
+package packman.controller.aloneList;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/list/alone/category")
+public class AloneListCategoryController {
+}
