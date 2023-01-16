1cf4b731c2a560d6e33d556c889e20f40f82ce79
dingding-21 <phj8221@naver.com>
2023-01-16 17:29:43 +0900

    feat: 엔티티 예시

---
+++ src/main/java/com/packman/server/entity/Folder.java
@@ -0,0 +1,26 @@
+package com.packman.server.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "folder")
+public class Folder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "folder_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
---
+++ src/main/java/com/packman/server/entity/User.java
@@ -0,0 +1,37 @@
+package com.packman.server.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "users")
+public class User extends TimeStamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(length = 50)
+    private String email;
+
+    @Column(length = 4)
+    private String nickname;
+
+    @Column(length = 1)
+    private String profileImage;
+
+    @Column
+    private boolean isDeleted = true;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Folder> folders = new ArrayList<>();
+}
