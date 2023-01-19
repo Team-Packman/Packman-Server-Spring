55bae185462943d625f483635c488e459924039f
kkl4846 <kkl4846@cau.ac.kr>
2023-01-20 00:29:46 +0900

    feat: 폴더 생성 controller 구현

--- src/main/java/packman/controller/FolderController.java
+++ src/main/java/packman/controller/FolderController.java
@@ -1,11 +1,31 @@
 package packman.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import packman.dto.folder.FolderRequestDto;
+import packman.service.FolderService;
+import packman.util.ResponseCode;
+import packman.util.ResponseMessage;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/folder")
 public class FolderController {
+    private final FolderService folderService;
+
+    @PostMapping
+    public ResponseEntity<ResponseMessage> createFolder(@RequestBody @Valid FolderRequestDto folderRequestDto, HttpServletRequest request) {
+        Long userId = 1L;
+        return ResponseMessage.toResponseEntity(
+                ResponseCode.CREATE_FOLDER_SUCCESS,
+                folderService.createFolder(folderRequestDto, userId)
+        );
+    }
 }
