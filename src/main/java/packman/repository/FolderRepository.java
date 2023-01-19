c1c2984669cd31e9daf67dae0f62f15390b511cb
kkl4846 <kkl4846@cau.ac.kr>
2023-01-20 00:31:22 +0900

    feat: 유저의 혼자/함께 폴더 조회 메소드 추가

--- src/main/java/packman/repository/FolderRepository.java
+++ src/main/java/packman/repository/FolderRepository.java
@@ -2,8 +2,13 @@ package packman.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import packman.dto.folder.FolderMapping;
 import packman.entity.Folder;
+
+import java.util.ArrayList;
 
 @Repository
 public interface FolderRepository extends JpaRepository<Folder, Long> {
+    ArrayList<FolderMapping> findByUserIdAndIsAlonedOrderByUserIdDesc(Long userId, boolean isAloned);
+
 }
