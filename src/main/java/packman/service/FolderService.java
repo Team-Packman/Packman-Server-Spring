bfcb28705894c2280b3859be0eca1ee0bdea653b
kkl4846 <kkl4846@cau.ac.kr>
2023-01-20 00:30:01 +0900

    feat: 폴더 생성 service 구현

--- src/main/java/packman/service/FolderService.java
+++ src/main/java/packman/service/FolderService.java
@@ -3,9 +3,47 @@ package packman.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import packman.dto.folder.FolderMapping;
+import packman.dto.folder.FolderRequestDto;
+import packman.dto.folder.FolderResponseDto;
+import packman.entity.Folder;
+import packman.entity.User;
+import packman.repository.FolderRepository;
+import packman.repository.UserRepository;
+import packman.util.CustomException;
+import packman.util.ResponseCode;
+
+import java.util.ArrayList;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class FolderService {
+
+    private final FolderRepository folderRepository;
+    private final UserRepository userRepository;
+
+    public FolderResponseDto createFolder(FolderRequestDto request, Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> new CustomException(ResponseCode.NO_USER)
+        );
+        String name = request.getName();
+
+        // validation
+        if (name.length() > 8) {
+            throw new CustomException(ResponseCode.CREATE_FOLDER_FAIL);
+        }
+
+        Folder folder = new Folder(request, user);
+        folderRepository.save(folder);
+
+        ArrayList<FolderMapping> aloneFolders = folderRepository.findByUserIdAndIsAlonedOrderByUserIdDesc(userId, true);
+        ArrayList<FolderMapping> togetherFolders = folderRepository.findByUserIdAndIsAlonedOrderByUserIdDesc(userId, false);
+
+        return new FolderResponseDto(
+                aloneFolders,
+                togetherFolders
+        );
+    }
+
 }
