585f45f14e0e14813c65f13723c6322f035e896e
dingding-21 <phj8221@naver.com>
2023-01-19 16:01:36 +0900

    feat: 유저 조회 controller 구현

--- src/main/java/packman/controller/UserController.java
+++ src/main/java/packman/controller/UserController.java
@@ -1,11 +1,29 @@
 package packman.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import packman.service.UserService;
+import packman.util.ResponseCode;
+import packman.util.ResponseMessage;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user")
 public class UserController {
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<ResponseMessage> getUserInfo(HttpServletRequest request) {
+        Long userId = 1L;  //  임시 userId 1
+
+        return ResponseMessage.toResponseEntity(
+                ResponseCode.SUCCESS_GET_USER,
+                userService.getUserInfo(userId)
+        );
+    }
 }
