1839d6a567aa18e07f07edf0b33cf92152dc615e
dingding-21 <phj8221@naver.com>
2023-01-21 02:37:50 +0900

    feat: 유저 탈퇴 controller 구현

--- src/main/java/packman/controller/UserController.java
+++ src/main/java/packman/controller/UserController.java
@@ -2,12 +2,14 @@ package packman.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import packman.service.UserService;
 import packman.util.ResponseCode;
 import packman.util.ResponseMessage;
+import packman.util.ResponseNonDataMessage;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -24,6 +26,17 @@ public class UserController {
         return ResponseMessage.toResponseEntity(
                 ResponseCode.SUCCESS_GET_USER,
                 userService.getUser(userId)
+        );
+    }
+
+    @DeleteMapping
+    public ResponseEntity<ResponseNonDataMessage> deleteUser(HttpServletRequest request) {
+        Long userId = 1L;  //  임시 userId 1
+
+        userService.deleteUser(userId);
+
+        return ResponseNonDataMessage.toResponseEntity(
+                ResponseCode.SUCCESS_DELETE_USER
         );
     }
 }
