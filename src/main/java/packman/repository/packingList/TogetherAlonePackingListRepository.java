086df754943ed6177ac9ddb7c4b14d6996d4f9e0
kkl4846 <kkl4846@cau.ac.kr>
2023-01-21 21:21:38 +0900

    feat: userId와 공통 함께 패킹으로 유저의 함께+혼자 패킹 찾기 메서드

--- src/main/java/packman/repository/packingList/TogetherAlonePackingListRepository.java
+++ src/main/java/packman/repository/packingList/TogetherAlonePackingListRepository.java
@@ -3,7 +3,9 @@ package packman.repository.packingList;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import packman.entity.packingList.TogetherAlonePackingList;
+import packman.entity.packingList.TogetherPackingList;
 
 @Repository
 public interface TogetherAlonePackingListRepository extends JpaRepository<TogetherAlonePackingList, Long> {
+    TogetherAlonePackingList findByTogetherPackingListAndAlonePackingListFolderPackingListFolderUserId(TogetherPackingList together, Long userId);
 }
