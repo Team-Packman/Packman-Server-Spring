b078b1cdec341049fa36cc0551689fc4ca14615e
kkl4846 <kkl4846@cau.ac.kr>
2023-01-21 21:17:05 +0900

    feat: 함께 패킹 초대 responsecode 추가

--- src/main/java/packman/util/ResponseCode.java
+++ src/main/java/packman/util/ResponseCode.java
@@ -12,9 +12,13 @@ public enum ResponseCode {
     NULL_VALUE(HttpStatus.BAD_REQUEST, false, "필요한 값이 없습니다"),
     NOT_FOUND(HttpStatus.NOT_FOUND, false, "존재하지 않는 자원"),
     BAD_REQUEST(HttpStatus.BAD_REQUEST, false, "잘못된 요청"),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, false, "서버 내부 오류입니다");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, false, "서버 내부 오류입니다"),
 
+    // packing_list
+    NO_LIST(HttpStatus.BAD_REQUEST, false, "존재하지 않는 패킹리스트입니다"),
 
+    // together_packing_list
+    SUCCESS_INVITE_TOGETHER_PACKING(HttpStatus.OK, true, "함께 패킹리스트 초대 성공");
 
     private final HttpStatus httpStatus;
     private final Boolean success;
