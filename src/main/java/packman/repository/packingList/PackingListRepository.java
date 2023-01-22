4d53051f225bdc22c2d0532f9f3c9eb4c006612c
kkl4846 <kkl4846@cau.ac.kr>
2023-01-23 02:32:47 +0900

    fix: packingList entity연결 에러

--- src/main/java/packman/repository/packingList/PackingListRepository.java
+++ src/main/java/packman/repository/packingList/PackingListRepository.java
@@ -2,8 +2,8 @@ package packman.repository.packingList;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import packman.entity.Folder;
+import packman.entity.packingList.PackingList;
 
 @Repository
-public interface PackingListRepository extends JpaRepository<Folder, Long> {
+public interface PackingListRepository extends JpaRepository<PackingList, Long> {
 }
