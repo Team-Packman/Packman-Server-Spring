7dd59af8a4b4ba0cb6ef34476e3932ea40805497
dingding-21 <phj8221@naver.com>
2023-01-23 15:59:30 +0900

    feat: 혼자 패킹리스트 폴더 조회 controller 구현

--- src/main/java/packman/controller/FolderController.java
+++ src/main/java/packman/controller/FolderController.java
@@ -1,11 +1,30 @@
 package packman.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import packman.service.FolderService;
+import packman.util.ResponseCode;
+import packman.util.ResponseMessage;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/folder")
 public class FolderController {
+    private final FolderService folderService;
+
+    @GetMapping("/alone")
+    public ResponseEntity<ResponseMessage> getAloneFolders(HttpServletRequest request) {
+        Long userId = 1L;  //  임시 userId 1
+
+        return ResponseMessage.toResponseEntity(
+                ResponseCode.SUCCESS_GET_TOGETHER_FOLDERS,
+                folderService.getAloneFolders(userId)
+        );
+    }
+
 }
