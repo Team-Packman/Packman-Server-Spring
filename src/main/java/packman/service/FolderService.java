825e5b8946a7236ac3c71465968ccbfce97b726c
dingding-21 <phj8221@naver.com>
2023-01-23 16:02:12 +0900

    feat: 혼자 패킹리스트 폴더 조회 service 구현

--- src/main/java/packman/service/FolderService.java
+++ src/main/java/packman/service/FolderService.java
@@ -3,9 +3,29 @@ package packman.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import packman.dto.folder.AloneFolderMapping;
+import packman.dto.folder.AloneFoldersResponseDto;
+import packman.repository.FolderRepository;
+import packman.repository.UserRepository;
+import packman.util.CustomException;
+import packman.util.ResponseCode;
+
+import java.util.ArrayList;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class FolderService {
+    private final UserRepository userRepository;
+    private final FolderRepository folderRepository;
+
+    public AloneFoldersResponseDto getAloneFolders(Long userId) {
+        userRepository.findByIdAndIsDeleted(userId, false).orElseThrow(
+                () -> new CustomException(ResponseCode.NO_USER)
+        );
+
+        ArrayList<AloneFolderMapping> aloneFolderMappings = folderRepository.findByUserIdAndIsAlonedOrderByCreatedAtDesc(userId, true);
+
+        return new AloneFoldersResponseDto(aloneFolderMappings);
+    }
 }
