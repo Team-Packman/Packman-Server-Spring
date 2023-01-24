34005e1e2e2e399ec9141928a6c526293ad94c8d
laalaa31 <jseoh4298@gmail.com>
2023-01-24 16:24:01 +0900

    feat: 유저 생성 Controller 추가

--- src/main/java/packman/controller/UserController.java
+++ src/main/java/packman/controller/UserController.java
@@ -1,11 +1,29 @@
 package packman.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import packman.dto.user.UserRequestDto;
+import packman.service.UserService;
+import packman.util.ResponseCode;
+import packman.util.ResponseMessage;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user")
 public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/profile")
+    public ResponseEntity<ResponseMessage> createUser(@RequestBody UserRequestDto userRequestDto) {
+        Long userId = 1L;
+        return ResponseMessage.toResponseEntity(
+                ResponseCode.CREATE_USER_SUCCESS,
+                userService.createUser(userRequestDto, userId)
+        );
+    }
+
 }
