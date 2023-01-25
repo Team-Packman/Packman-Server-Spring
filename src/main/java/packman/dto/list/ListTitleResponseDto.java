aaeda81ee6ceb25d2045fe0505dd4caf18e90f73
laalaa31 <jseoh4298@gmail.com>
2023-01-26 00:07:32 +0900

    feat: 패킹리스트 제목 수정 service 구현

--- src/main/java/packman/dto/list/ListTitleResponseDto.java
+++ src/main/java/packman/dto/list/ListTitleResponseDto.java
@@ -1,10 +1,11 @@
 package packman.dto.list;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
+@AllArgsConstructor
 public class ListTitleResponseDto {
     private String id;
     private String title;
--- src/main/java/packman/service/ListService.java
+++ src/main/java/packman/service/ListService.java
@@ -5,19 +5,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import packman.dto.list.ListTitleRequestDto;
 import packman.dto.list.ListTitleResponseDto;
-import packman.entity.packingList.PackingList;
-import packman.entity.packingList.TogetherAlonePackingList;
-import packman.entity.packingList.TogetherPackingList;
 import packman.repository.UserRepository;
-import packman.repository.packingList.AlonePackingListRepository;
 import packman.repository.packingList.PackingListRepository;
 import packman.repository.packingList.TogetherAlonePackingListRepository;
-import packman.repository.packingList.TogetherPackingListRepository;
 import packman.util.CustomException;
 import packman.util.ResponseCode;
-
-import java.util.ArrayList;
-import java.util.Optional;
 
 @Service
 @Transactional
@@ -26,25 +18,26 @@ public class ListService {
     private final UserRepository userRepository;
     private final PackingListRepository listRepository;
     private final TogetherAlonePackingListRepository togetherAlonePackingListRepository;
-    private final AlonePackingListRepository alonePackingListRepository;
-    private final TogetherPackingListRepository togetherPackingListRepository;
 
     public ListTitleResponseDto updateTitle(ListTitleRequestDto listTitleRequestDto, Long userId) {
-        Long listId=Long.parseLong(listTitleRequestDto.getId());
-        String title= listTitleRequestDto.getTitle();
+        Long listId = Long.parseLong(listTitleRequestDto.getId());
+        String title = listTitleRequestDto.getTitle();
 
         //유저 검증
         userRepository.findByIdAndIsDeleted(userId, false).orElseThrow(
                 () -> new CustomException(ResponseCode.NO_USER)
         );
 
-        if(listTitleRequestDto.getIsAloned()) {// 혼자 패킹리스트
-            listRepository.findByIdAndIsDeleted(listId, false).ifPresentOrElse(t->{
-                t.setTitle(title);
-            }, ()->new CustomException(ResponseCode.NO_LIST));
+        if (!listTitleRequestDto.getIsAloned()) {
+            listId = togetherAlonePackingListRepository.findById(listId).orElseThrow(
+                    () -> new CustomException(ResponseCode.NO_LIST))
+                    .getTogetherPackingList().getPackingList().getId();
+        }
 
-        }
-        else{ //함께 패킹리스트
-        }
+        listRepository.findByIdAndIsDeleted(listId, false).ifPresentOrElse(t -> {
+            t.setTitle(title);
+        }, () -> {throw new CustomException(ResponseCode.NO_LIST);});
+
+        return new ListTitleResponseDto(listTitleRequestDto.getId(), title);
     }
 }
