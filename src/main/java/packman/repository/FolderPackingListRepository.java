7d5ea216547e17eba15768a0ee8b53142187bb36
dingding-21 <phj8221@naver.com>
2023-01-26 06:42:50 +0900

    feat: FolderPackingList에서 혼자 패킹리스트 id 배열을 받아오도록 반환 값 수정 및 DTO 추가

--- src/main/java/packman/repository/FolderPackingListRepository.java
+++ src/main/java/packman/repository/FolderPackingListRepository.java
@@ -2,12 +2,12 @@ package packman.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import packman.dto.list.ListIdDtoMapping;
+import packman.dto.folder.FolderAloneListMapping;
 import packman.entity.FolderPackingList;
 
 import java.util.List;
 
 @Repository
 public interface FolderPackingListRepository extends JpaRepository<FolderPackingList, Long> {
-    List<ListIdDtoMapping> findByFolderIdAndAlonePackingList_IsAlonedAndAlonePackingList_PackingList_IsDeletedOrderByIdDesc(Long folderId, boolean isAloned, boolean isDeleted);
+    List<FolderAloneListMapping> findByFolderIdAndAlonePackingList_IsAlonedAndAlonePackingList_PackingList_IsDeletedOrderByIdDesc(Long folderId, boolean isAloned, boolean isDeleted);
 }
