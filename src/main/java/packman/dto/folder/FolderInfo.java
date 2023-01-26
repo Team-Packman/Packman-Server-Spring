0e433d422ac87153087ec93b7eba01fb040cf465
kkl4846 <kkl4846@cau.ac.kr>
2023-01-26 20:36:04 +0900

    feat: response 순서 부여

--- src/main/java/packman/dto/folder/FolderInfo.java
+++ src/main/java/packman/dto/folder/FolderInfo.java
@@ -1,11 +1,13 @@
 package packman.dto.folder;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@JsonPropertyOrder({ "id", "name", "listNum" })
 public class FolderInfo {
     String id;
     String name;
