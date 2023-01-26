e6d6e1d733131c0aa6a1289eb24a8a8ba0ca57a8
kkl4846 <kkl4846@cau.ac.kr>
2023-01-26 22:14:56 +0900

    chore: 생성자 어노테이션 추가

--- src/main/java/packman/dto/folder/FolderInfo.java
+++ src/main/java/packman/dto/folder/FolderInfo.java
@@ -2,20 +2,15 @@ package packman.dto.folder;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@JsonPropertyOrder({ "id", "name", "listNum" })
+@AllArgsConstructor
 public class FolderInfo {
     String id;
     String name;
     String listNum;
 
-    public FolderInfo(String id, String name, String listNum) {
-        this.id = id;
-        this.name = name;
-        this.listNum = listNum;
-    }
 }
