b71da27b4b2b369383960bbeb55ea9cf53b5ccb9
laalaa31 <jseoh4298@gmail.com>
2023-01-26 10:52:07 +0900

    feat: 패킹리스트 출발날짜 수정 Dto 추가

---
+++ src/main/java/packman/dto/list/DepartureDateResponseDto.java
@@ -0,0 +1,11 @@
+package packman.dto.list;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class DepartureDateResponseDto {
+    private String id;
+    private String title;
+}
