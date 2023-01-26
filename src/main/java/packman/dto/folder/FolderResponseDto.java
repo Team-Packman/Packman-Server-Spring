fa4e001177a8f5674be0caed9611c15706e7c05f
kkl4846 <kkl4846@cau.ac.kr>
2023-01-26 20:52:20 +0900

    chore: 자동 생성자 어노테이션 추가

--- src/main/java/packman/dto/folder/FolderResponseDto.java
+++ src/main/java/packman/dto/folder/FolderResponseDto.java
@@ -1,18 +1,14 @@
 package packman.dto.folder;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 public class FolderResponseDto {
     private List<FolderInfo> aloneFolder;
     private List<FolderInfo> togetherFolder;
 
-
-    public FolderResponseDto(List<FolderInfo> aloneFolder, List<FolderInfo> togetherFolder) {
-        this.aloneFolder = aloneFolder;
-        this.togetherFolder = togetherFolder;
-    }
 }
