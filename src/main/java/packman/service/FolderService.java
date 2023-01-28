d47ad68c92c50bf5c5b4234c76ffe83088a190ba
dingding-21 <phj8221@naver.com>
2023-01-28 18:01:31 +0900

    feat: responseBody에서 aloneFolder로 감싼 것 제거

--- src/main/java/packman/service/FolderService.java
+++ src/main/java/packman/service/FolderService.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import packman.dto.folder.AloneFolderMapping;
-import packman.dto.folder.AloneFoldersResponseDto;
 import packman.repository.FolderRepository;
 import packman.repository.UserRepository;
 import packman.util.CustomException;
@@ -19,13 +18,11 @@ public class FolderService {
     private final UserRepository userRepository;
     private final FolderRepository folderRepository;
 
-    public AloneFoldersResponseDto getAloneFolders(Long userId) {
+    public ArrayList<AloneFolderMapping> getAloneFolders(Long userId) {
         userRepository.findByIdAndIsDeleted(userId, false).orElseThrow(
                 () -> new CustomException(ResponseCode.NO_USER)
         );
 
-        ArrayList<AloneFolderMapping> aloneFolderMappings = folderRepository.findByUserIdAndIsAlonedOrderByIdDesc(userId, true);
-
-        return new AloneFoldersResponseDto(aloneFolderMappings);
+        return folderRepository.findByUserIdAndIsAlonedOrderByIdDesc(userId, true);
     }
 }
