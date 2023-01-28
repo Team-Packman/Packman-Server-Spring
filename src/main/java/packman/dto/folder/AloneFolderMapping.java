fc079ea16289e40de833bbc02e098d9a011c7e83
dingding-21 <phj8221@naver.com>
2023-01-28 17:56:37 +0900

    feat: Mapping 후 id, name 순서 지정

--- src/main/java/packman/dto/folder/AloneFolderMapping.java
+++ src/main/java/packman/dto/folder/AloneFolderMapping.java
@@ -1,5 +1,8 @@
 package packman.dto.folder;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({"id", "name"})
 public interface AloneFolderMapping {
     String getId();
 
