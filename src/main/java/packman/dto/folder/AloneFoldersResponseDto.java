e9bcc5b3c8975987fe7fa941dabcb90ff3e64cf2
dingding-21 <phj8221@naver.com>
2023-01-28 17:55:56 +0900

    refactor: @AllArgsConstructor으로 생성자 대체

--- src/main/java/packman/dto/folder/AloneFoldersResponseDto.java
+++ src/main/java/packman/dto/folder/AloneFoldersResponseDto.java
@@ -1,14 +1,12 @@
 package packman.dto.folder;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.ArrayList;
 
 @Data
+@AllArgsConstructor
 public class AloneFoldersResponseDto {
     private ArrayList<AloneFolderMapping> aloneFolder;
-
-    public AloneFoldersResponseDto(ArrayList<AloneFolderMapping> aloneFolderMappings) {
-        this.aloneFolder = aloneFolderMappings;
-    }
 }
