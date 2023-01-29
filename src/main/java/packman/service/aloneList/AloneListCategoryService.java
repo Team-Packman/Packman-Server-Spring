dcc59a5d76078a45eb63b2e8691a4b2fd5e21bf8
kkl4846 <kkl4846@cau.ac.kr>
2023-01-29 15:33:19 +0900

    feat: 혼자 패킹 카테고리 수정 서비스 구현

--- src/main/java/packman/service/aloneList/AloneListCategoryService.java
+++ src/main/java/packman/service/aloneList/AloneListCategoryService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import packman.dto.category.CategoryCreateDto;
 import packman.dto.category.CategoryResponseDto;
+import packman.dto.category.CategoryUpdateDto;
 import packman.entity.Category;
 import packman.entity.packingList.PackingList;
 import packman.repository.CategoryRepository;
@@ -46,6 +47,41 @@ public class AloneListCategoryService {
 
         // response
         CategoryResponseDto categoryResponseDto = packingListRepository.findByIdAndTitle(Long.parseLong(categoryCreateDto.getListId()), packingList.getTitle());
+        return categoryResponseDto;
+    }
+
+    public CategoryResponseDto updateCategory(CategoryUpdateDto categoryUpdateDto, Long userId) {
+        // 카테고리 exceed_len
+        if (categoryUpdateDto.getName().length() > 12) {
+            throw new CustomException(ResponseCode.EXCEED_LEN);
+        }
+        // no_list
+        PackingList packingList = packingListRepository.findByIdAndIsDeleted(Long.parseLong(categoryUpdateDto.getListId()), false).orElseThrow(
+                () -> new CustomException(ResponseCode.NO_LIST)
+        );
+        // no_category
+        Category category = categoryRepository.findById(Long.parseLong(categoryUpdateDto.getId())).orElseThrow(
+                () -> new CustomException(ResponseCode.NO_CATEGORY)
+        );
+        // no_list_category
+        if (category.getPackingList().getId() != Long.parseLong(categoryUpdateDto.getListId())) {
+            throw new CustomException(ResponseCode.NO_LIST_CATEGORY);
+        }
+
+        // duplicate_category
+        List<Category> categorys = packingList.getCategory();
+        categorys.stream().forEach(c -> {
+            if (c.getName().equals(categoryUpdateDto.getName()) && c.getId() != Long.parseLong(categoryUpdateDto.getId())) {
+                throw new CustomException(ResponseCode.DUPLICATED_CATEGORY);
+            }
+        });
+
+        // update
+        category.setName(categoryUpdateDto.getName());
+        categoryRepository.save(category);
+
+        // response
+        CategoryResponseDto categoryResponseDto = packingListRepository.findByIdAndTitle(Long.parseLong(categoryUpdateDto.getListId()), packingList.getTitle());
         return categoryResponseDto;
     }
 }
