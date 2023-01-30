8019013036d9316d5e5936d5e5acb990b43973cc
dingding-21 <phj8221@naver.com>
2023-01-30 16:07:11 +0900

    feat: 혼자 패킹 짐 수정 validate함수 구현

--- src/main/java/packman/validator/IdValidator.java
+++ src/main/java/packman/validator/IdValidator.java
@@ -1,8 +1,10 @@
 package packman.validator;
 
 import packman.entity.Category;
+import packman.entity.Pack;
 import packman.entity.packingList.AlonePackingList;
 import packman.repository.CategoryRepository;
+import packman.repository.PackRepository;
 import packman.repository.UserRepository;
 import packman.repository.packingList.AlonePackingListRepository;
 import packman.util.CustomException;
@@ -24,6 +26,12 @@ public class IdValidator {
     public static Category validateCategoryId(CategoryRepository categoryRepository, Long categoryId) {
         return categoryRepository.findById(categoryId).orElseThrow(
                 () -> new CustomException(ResponseCode.NO_CATEGORY)
+        );
+    }
+
+    public static Pack validatePackId(PackRepository packRepository, Long packId) {
+        return packRepository.findById(packId).orElseThrow(
+                () -> new CustomException(ResponseCode.NO_PACK)
         );
     }
 }
