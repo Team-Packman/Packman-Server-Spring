138a292fe2153684319e2995c3d29fb7a005946d
dingding-21 <phj8221@naver.com>
2023-01-30 16:08:11 +0900

    feat: 혼자 패킹 짐 수정 service validate 부분 구현

--- src/main/java/packman/service/aloneList/AloneListPackService.java
+++ src/main/java/packman/service/aloneList/AloneListPackService.java
@@ -5,16 +5,19 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import packman.dto.list.ListResponseMapping;
 import packman.dto.pack.PackCreateDto;
+import packman.dto.pack.PackUpdateDto;
 import packman.entity.Category;
 import packman.entity.Pack;
 import packman.entity.packingList.AlonePackingList;
 import packman.repository.CategoryRepository;
+import packman.repository.PackRepository;
 import packman.repository.UserRepository;
 import packman.repository.packingList.AlonePackingListRepository;
 import packman.repository.packingList.PackingListRepository;
 
 import static packman.validator.IdValidator.*;
 import static packman.validator.LengthValidator.validatePackLength;
+import static packman.validator.Validator.validateCategoryPack;
 import static packman.validator.Validator.validateListCategory;
 
 @Service
@@ -25,6 +28,7 @@ public class AloneListPackService {
     private final AlonePackingListRepository alonePackingListRepository;
     private final CategoryRepository categoryRepository;
     private final PackingListRepository packingListRepository;
+    private final PackRepository packRepository;
 
     public ListResponseMapping createPack(PackCreateDto packCreateDto, Long userId) {
         Long listId = Long.valueOf(packCreateDto.getListId());
@@ -42,5 +46,23 @@ public class AloneListPackService {
         category.addPack(pack);
 
         return packingListRepository.findByIdAndTitle(listId, alonePackingList.getPackingList().getTitle());
+    }
+
+    public ListResponseMapping updatePack(PackUpdateDto packUpdateDto, Long userId) {
+        Long listId = Long.valueOf(packUpdateDto.getListId());
+        Long categoryId = Long.valueOf(packUpdateDto.getCategoryId());
+        String packName = packUpdateDto.getName();
+        Long packId = Long.valueOf(packUpdateDto.getId());
+
+        validateUserId(userRepository, userId);
+
+        AlonePackingList alonePackingList = validateAloneListId(alonePackingListRepository, listId);
+        Category category = validateCategoryId(categoryRepository, categoryId);
+        Pack pack = validatePackId(packRepository, packId);
+
+        validatePackLength(packName);
+
+        validateListCategory(listId, category);
+        validateCategoryPack(categoryId, pack);
     }
 }
