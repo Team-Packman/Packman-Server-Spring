bdc6ccbb0e6255e806b08032d7d914ede4c0bd89
kkl4846 <kkl4846@cau.ac.kr>
2023-01-31 01:18:13 +0900

    feat: 함께 패킹 카테고리 수정 service

--- src/main/java/packman/service/togetherList/TogetherListCategoryService.java
+++ src/main/java/packman/service/togetherList/TogetherListCategoryService.java
@@ -5,19 +5,19 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import packman.dto.category.CategoryCreateDto;
 import packman.dto.category.CategoryResponseDto;
+import packman.dto.category.CategoryUpdateDto;
 import packman.entity.Category;
 import packman.entity.UserGroup;
 import packman.entity.packingList.PackingList;
+import packman.repository.CategoryRepository;
 import packman.repository.packingList.PackingListRepository;
 import packman.util.CustomException;
 import packman.util.ResponseCode;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static packman.validator.DuplicatedValidator.validateDuplicatedCategory;
-import static packman.validator.IdValidator.validatePackingListId;
-import static packman.validator.IdValidator.validateUserMemberId;
+import static packman.validator.IdValidator.*;
 import static packman.validator.LengthValidator.validateCategoryLength;
 
 @Service
@@ -25,6 +25,7 @@ import static packman.validator.LengthValidator.validateCategoryLength;
 @RequiredArgsConstructor
 public class TogetherListCategoryService {
     private final PackingListRepository packingListRepository;
+    private final CategoryRepository categoryRepository;
 
     public CategoryResponseDto createCategory(CategoryCreateDto categoryCreateDto, Long userId) {
 
@@ -36,7 +37,7 @@ public class TogetherListCategoryService {
 
         // no_member_user
         List<UserGroup> userGroups = packingList.getTogetherPackingList().getGroup().getUserGroups();
-        validateUserMemberId( userGroups, userId);
+        validateUserMemberId(userGroups, userId);
 
         // duplicate_category
         validateDuplicatedCategory(packingList, categoryCreateDto.getName(), null);
@@ -49,6 +50,37 @@ public class TogetherListCategoryService {
 
         // response
         CategoryResponseDto categoryResponseDto = packingListRepository.findByIdAndTitle(Long.parseLong(categoryCreateDto.getListId()), packingList.getTitle());
+        return categoryResponseDto;
+    }
+
+    public CategoryResponseDto updateCategory(CategoryUpdateDto categoryUpdateDto, Long userId) {
+
+        // 카테고리 exceed_len
+        validateCategoryLength(categoryUpdateDto.getName());
+
+        // no_list
+        PackingList packingList = validatePackingListId(packingListRepository, Long.parseLong(categoryUpdateDto.getListId()));
+
+        // no_category
+        Category category = validateCategoryId(categoryRepository, Long.parseLong(categoryUpdateDto.getId()));
+
+        // no_list_category
+        if (category.getPackingList().getId() != Long.parseLong(categoryUpdateDto.getListId())) {
+            throw new CustomException(ResponseCode.NO_LIST_CATEGORY);
+        }
+
+        // no_member_user
+        List<UserGroup> userGroups = packingList.getTogetherPackingList().getGroup().getUserGroups();
+        validateUserMemberId(userGroups, userId);
+
+        // duplicate_category
+        validateDuplicatedCategory(packingList, categoryUpdateDto.getName(), Long.parseLong(categoryUpdateDto.getId()));
+
+        // update
+        category.setName(categoryUpdateDto.getName());
+
+        // response
+        CategoryResponseDto categoryResponseDto = packingListRepository.findByIdAndTitle(Long.parseLong(categoryUpdateDto.getListId()), packingList.getTitle());
         return categoryResponseDto;
     }
 
