f2fe4f6ce43c7fd8b2f9ad396020e53baa6a62c3
kkl4846 <kkl4846@cau.ac.kr>
2023-01-30 23:13:56 +0900

    feat: 카테고리 중복 validator 생성, 수정 함께 쓰도록 매개변수 추가

--- src/main/java/packman/validator/DuplicatedValidator.java
+++ src/main/java/packman/validator/DuplicatedValidator.java
@@ -6,13 +6,16 @@ import packman.util.CustomException;
 import packman.util.ResponseCode;
 
 import java.util.List;
+import java.util.Optional;
 
 public class DuplicatedValidator {
-    public static void validateDuplicatedCategory(PackingList packingList, String categoryName) {
+    public static void validateDuplicatedCategory(PackingList packingList, String categoryName, Long categoryId) {
         List<Category> categorys = packingList.getCategory();
         categorys.stream().forEach(category -> {
             if (category.getName().equals(categoryName)) {
-                throw new CustomException(ResponseCode.DUPLICATED_CATEGORY);
+                if(categoryId != null && category.getId() != categoryId) {
+                    throw new CustomException(ResponseCode.DUPLICATED_CATEGORY);
+                }
             }
         });
     }
--- src/main/java/packman/validator/IdValidator.java
+++ src/main/java/packman/validator/IdValidator.java
@@ -1,7 +1,9 @@
 package packman.validator;
 
+import packman.entity.Category;
 import packman.entity.UserGroup;
 import packman.entity.packingList.PackingList;
+import packman.repository.CategoryRepository;
 import packman.repository.packingList.PackingListRepository;
 import packman.util.CustomException;
 import packman.util.ResponseCode;
@@ -18,10 +20,16 @@ public class IdValidator {
 
     public static void validateUserMemberId(List<UserGroup> userGroups, Long userId) {
         List<Long> userIdInGroup = userGroups.stream()
-                .map( userGroup -> userGroup.getUser().getId())
+                .map(userGroup -> userGroup.getUser().getId())
                 .collect(Collectors.toList());
         if (!userIdInGroup.contains(userId)) {
             throw new CustomException(ResponseCode.NO_MEMBER_USER);
         }
+    }
+
+    public static Category validateCategoryId(CategoryRepository categoryRepository, Long categoryId) {
+        return categoryRepository.findById(categoryId).orElseThrow(
+                () -> new CustomException(ResponseCode.NO_CATEGORY)
+        );
     }
 }
