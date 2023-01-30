ce32ef367d0b7664a9c55589fd47666596a0193e
kkl4846 <kkl4846@cau.ac.kr>
2023-01-31 00:57:12 +0900

    feat: 카테고리 중복 validator 함께 패킹에도 적용

--- src/main/java/packman/validator/DuplicatedValidator.java
+++ src/main/java/packman/validator/DuplicatedValidator.java
@@ -6,16 +6,13 @@ import packman.util.CustomException;
 import packman.util.ResponseCode;
 
 import java.util.List;
-import java.util.Optional;
 
 public class DuplicatedValidator {
     public static void validateDuplicatedCategory(PackingList packingList, String categoryName, Long categoryId) {
         List<Category> categorys = packingList.getCategory();
         categorys.stream().forEach(category -> {
-            if (category.getName().equals(categoryName)) {
-                if(categoryId != null && category.getId() != categoryId) {
-                    throw new CustomException(ResponseCode.DUPLICATED_CATEGORY);
-                }
+            if (category.getName().equals(categoryName) && categoryId != category.getId()) {
+                throw new CustomException(ResponseCode.DUPLICATED_CATEGORY);
             }
         });
     }
