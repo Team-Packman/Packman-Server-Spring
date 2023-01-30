96160f3134853b5b1109a129ba691107b9577653
dingding-21 <phj8221@naver.com>
2023-01-30 16:28:27 +0900

    feat: Pack에 @DynamicUpdate annotation 추가

--- src/main/java/packman/entity/Pack.java
+++ src/main/java/packman/entity/Pack.java
@@ -3,6 +3,7 @@ package packman.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
 
@@ -12,6 +13,7 @@ import static javax.persistence.FetchType.LAZY;
 @Getter
 @Setter
 @NoArgsConstructor
+@DynamicUpdate
 public class Pack {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
