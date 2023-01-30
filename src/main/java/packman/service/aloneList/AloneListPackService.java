e96e936590817dcf7f818ae628371e4606e84d2c
dingding-21 <phj8221@naver.com>
2023-01-30 12:26:32 +0900

    feat: 혼자 패킹 짐 생성 service validate 부분 구현

--- src/main/java/packman/service/aloneList/AloneListPackService.java
+++ src/main/java/packman/service/aloneList/AloneListPackService.java
@@ -3,9 +3,38 @@ package packman.service.aloneList;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import packman.dto.category.CategoryResponseDto;
+import packman.dto.pack.PackCreateDto;
+import packman.entity.Category;
+import packman.entity.packingList.AlonePackingList;
+import packman.repository.CategoryRepository;
+import packman.repository.UserRepository;
+import packman.repository.packingList.AlonePackingListRepository;
+import packman.repository.packingList.PackingListRepository;
+
+import static packman.validator.IdValidator.*;
+import static packman.validator.LengthValidator.validatePackLength;
+import static packman.validator.Validator.validateListCategory;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class AloneListPackService {
+    private final UserRepository userRepository;
+    private final AlonePackingListRepository alonePackingListRepository;
+    private final CategoryRepository categoryRepository;
+    private final PackingListRepository packingListRepository;
+
+    public CategoryResponseDto createPack(PackCreateDto packCreateDto, Long userId) {
+        Long listId = Long.valueOf(packCreateDto.getListId());
+        Long categoryId = Long.valueOf(packCreateDto.getCategoryId());
+        String packName = packCreateDto.getName();
+
+        validateUserId(userRepository, userId);
+        AlonePackingList alonePackingList = validateAloneListId(alonePackingListRepository, listId);
+        Category category = validateCategoryId(categoryRepository, categoryId);
+
+        validatePackLength(packName);
+        validateListCategory(listId, category);
+    }
 }
