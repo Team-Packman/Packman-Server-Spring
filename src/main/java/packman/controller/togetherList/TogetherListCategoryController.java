59e7f61788d25d408ca025f0b2e771b4dcbc875f
kkl4846 <kkl4846@cau.ac.kr>
2023-01-31 01:17:55 +0900

    feat: 함께 패킹 카테고리 수정 controller구현

--- src/main/java/packman/controller/togetherList/TogetherListCategoryController.java
+++ src/main/java/packman/controller/togetherList/TogetherListCategoryController.java
@@ -2,11 +2,9 @@ package packman.controller.togetherList;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import packman.dto.category.CategoryCreateDto;
+import packman.dto.category.CategoryUpdateDto;
 import packman.service.togetherList.TogetherListCategoryService;
 import packman.util.ResponseCode;
 import packman.util.ResponseMessage;
@@ -27,6 +25,15 @@ public class TogetherListCategoryController {
         return ResponseMessage.toResponseEntity(
                 ResponseCode.SUCCESS_CREATE_TOGETHER_CATEGORY,
                 togetherListCategoryService.createCategory(categoryCreateDto, userId)
+        );
+    }
+
+    @PatchMapping
+    public ResponseEntity<ResponseMessage> updateCategory(@RequestBody @Valid CategoryUpdateDto categoryUpdateDto, HttpServletRequest request) {
+        Long userId = 1L;
+        return ResponseMessage.toResponseEntity(
+                ResponseCode.SUCCESS_UPDATE_TOGETHER_CATEGORY,
+                togetherListCategoryService.updateCategory(categoryUpdateDto, userId)
         );
     }
 }
