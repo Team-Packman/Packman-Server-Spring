ff51217e47602301a6a1417eb16ec8f0b229eaf8
kkl4846 <kkl4846@cau.ac.kr>
2023-01-31 17:12:53 +0900

    feat: 함께 패킹 카테고리 삭제 responseCode 추가

--- src/main/java/packman/util/ResponseCode.java
+++ src/main/java/packman/util/ResponseCode.java
@@ -40,6 +40,7 @@ public enum ResponseCode {
     // togetherPackingList
     SUCCESS_CREATE_TOGETHER_CATEGORY(HttpStatus.OK, true, "함께 패킹리스트 카테고리 생성 성공"),
     SUCCESS_UPDATE_TOGETHER_CATEGORY(HttpStatus.OK, true, "함께 패킹리스트 카테고리 수정 성공"),
+    SUCCESS_DELETE_TOGETHER_CATEGORY(HttpStatus.OK, true, "함께 패킹리스트 카테고리 삭제 성공"),
     // category
     DUPLICATED_CATEGORY(HttpStatus.BAD_REQUEST, false, "중복된 카테고리 명입니다"),
     NO_CATEGORY(HttpStatus.BAD_REQUEST, false, "존재하지 않는 카테고리입니다"),
