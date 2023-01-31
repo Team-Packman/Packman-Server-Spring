79258543c30d2bb0bb814b87185bd2f4e12bb49b
kkl4846 <kkl4846@cau.ac.kr>
2023-01-31 17:58:00 +0900

    chore: userId validator User로 반환하도록 수정

--- src/main/java/packman/validator/IdValidator.java
+++ src/main/java/packman/validator/IdValidator.java
@@ -1,12 +1,13 @@
 package packman.validator;
 
+import packman.entity.User;
 import packman.repository.UserRepository;
 import packman.util.CustomException;
 import packman.util.ResponseCode;
 
 public class IdValidator {
-    public static void validateUserId(UserRepository userRepository, Long userId) {
-        userRepository.findByIdAndIsDeleted(userId, false).orElseThrow(
+    public static User validateUserId(UserRepository userRepository, Long userId) {
+        return userRepository.findByIdAndIsDeleted(userId, false).orElseThrow(
                 () -> new CustomException(ResponseCode.NO_USER)
         );
     }
