9d9ca235cc7f1fab15cc626397f93962b49b484b
dingding-21 <phj8221@naver.com>
2023-02-01 22:35:45 +0900

    feat: 함께 패킹리스트 짐 수정 controller 구현

--- src/main/java/packman/controller/togetherList/TogetherListPackController.java
+++ src/main/java/packman/controller/togetherList/TogetherListPackController.java
@@ -1,11 +1,32 @@
 package packman.controller.togetherList;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import packman.dto.pack.PackUpdateDto;
+import packman.service.PackService;
+import packman.util.ResponseCode;
+import packman.util.ResponseMessage;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/list/together/pack")
 public class TogetherListPackController {
+    private final PackService packService;
+
+    @PatchMapping
+    public ResponseEntity<ResponseMessage> updatePack(@RequestBody @Valid PackUpdateDto packUpdateDto, HttpServletRequest request) {
+        Long userId = 1L;
+
+        return ResponseMessage.toResponseEntity(
+                ResponseCode.SUCCESS_UPDATE_TOGETHER_PACK,
+                packService.updateTogetherPack(packUpdateDto, userId)
+        );
+    }
 }
