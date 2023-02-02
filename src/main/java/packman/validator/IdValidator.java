62632cb42c5c1a8f077707306fe07ffbfaf9f24f
laalaa31 <jseoh4298@gmail.com>
2023-02-03 01:58:07 +0900

    chore: userId validator return 타입 변경

--- src/main/java/packman/validator/IdValidator.java
+++ src/main/java/packman/validator/IdValidator.java
@@ -1,13 +1,16 @@
 package packman.validator;
 
+import packman.entity.User;
 import packman.repository.UserRepository;
 import packman.util.CustomException;
 import packman.util.ResponseCode;
 
 public class IdValidator {
-    public static void validateUserId(UserRepository userRepository, Long userId) {
-        userRepository.findByIdAndIsDeleted(userId, false).orElseThrow(
+    public static User validateUserId(UserRepository userRepository, Long userId) {
+        User user = userRepository.findByIdAndIsDeleted(userId, false).orElseThrow(
                 () -> new CustomException(ResponseCode.NO_USER)
         );
+
+        return user;
     }
 }
