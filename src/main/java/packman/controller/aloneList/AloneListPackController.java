4b038cccf3155581965a316cb15c90bf039e5770
dingding-21 <phj8221@naver.com>
2023-02-05 12:15:36 +0900

    chore: alonePackService에서 packService로 수정

--- src/main/java/packman/controller/aloneList/AloneListPackController.java
+++ src/main/java/packman/controller/aloneList/AloneListPackController.java
@@ -6,6 +6,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import packman.dto.pack.PackCreateDto;
 import packman.dto.pack.PackUpdateDto;
+import packman.service.PackService;
 import packman.util.ResponseCode;
 import packman.util.ResponseMessage;
 import packman.util.ResponseNonDataMessage;
@@ -24,6 +25,7 @@ public class AloneListPackController {
     @PostMapping
     public ResponseEntity<ResponseMessage> createPack(@RequestBody @Valid PackCreateDto packCreateDto, HttpServletRequest request) {
         Long userId = 1L;
+        
         return ResponseMessage.toResponseEntity(
                 ResponseCode.SUCCESS_CREATE_ALONE_PACK,
                 packService.createAlonePack(packCreateDto, userId)
@@ -33,9 +35,10 @@ public class AloneListPackController {
     @PatchMapping
     public ResponseEntity<ResponseMessage> updatePack(@RequestBody @Valid PackUpdateDto packUpdateDto, HttpServletRequest request) {
         Long userId = 1L;
+
         return ResponseMessage.toResponseEntity(
                 ResponseCode.SUCCESS_UPDATE_ALONE_PACK,
-                aloneListPackService.updatePack(packUpdateDto, userId)
+                packService.updateAlonePack(packUpdateDto, userId)
         );
     }
 
@@ -43,7 +46,8 @@ public class AloneListPackController {
     public ResponseEntity<ResponseNonDataMessage> deletePack(
             @PathVariable @NotBlank String listId, @PathVariable @NotBlank String categoryId, @PathVariable @NotBlank String packId, HttpServletRequest request) {
         Long userId = 1L;
-        aloneListPackService.deletePack(Long.valueOf(listId), Long.valueOf(categoryId), Long.valueOf(packId), userId);
+
+        packService.deleteAlonePack(Long.valueOf(listId), Long.valueOf(categoryId), Long.valueOf(packId), userId);
 
         return ResponseNonDataMessage.toResponseEntity(
                 ResponseCode.SUCCESS_DELETE_ALONE_PACK
