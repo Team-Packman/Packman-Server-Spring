4b0e53c5fa755982ddedb377a1385cc935463750
dingding-21 <phj8221@naver.com>
2023-02-05 19:29:57 +0900

    feat: userId로 혼자패킹리스트들 조회하는 jpa 메서드 추가

--- src/main/java/packman/repository/packingList/AlonePackingListRepository.java
+++ src/main/java/packman/repository/packingList/AlonePackingListRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import packman.entity.packingList.AlonePackingList;
 
+import java.util.List;
+
 @Repository
 public interface AlonePackingListRepository extends JpaRepository<AlonePackingList, Long> {
+    List<AlonePackingList> findByFolderPackingList_Folder_UserIdOrderByIdDesc(Long userId);
 }
