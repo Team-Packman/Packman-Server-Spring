3e19a0e26a8b4e02dbe8aad3e232ab255459cf96
laalaa31 <jseoh4298@gmail.com>
2023-02-06 03:39:32 +0900

    feat: 혼자패킹 템플릿 리스트 조회 controller 구현

--- src/main/java/packman/controller/TemplateController.java
+++ src/main/java/packman/controller/TemplateController.java
@@ -1,11 +1,27 @@
 package packman.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import packman.service.TemplateService;
+import packman.util.ResponseCode;
+import packman.util.ResponseMessage;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/template")
 public class TemplateController {
+    @GetMapping("/alone")
+    public ResponseEntity<ResponseMessage> getAloneTemplateList(HttpServletRequest request) {
+        Long userId = 1L;
+
+        return ResponseMessage.toResponseEntity(
+                ResponseCode.SUCCESS_GET_ALONE_TEMPLATE_LIST,
+                TemplateService.getAloneTemplateList(userId)
+        );
+    }
 }
