5f730545552c6a067dc0e80810968333e08a9d67
kkl4846 <kkl4846@cau.ac.kr>
2023-02-05 21:47:22 +0900

    feat: 멤버 초대 controller 구현

--- src/main/java/packman/controller/togetherList/TogetherListController.java
+++ src/main/java/packman/controller/togetherList/TogetherListController.java
@@ -3,13 +3,13 @@ package packman.controller.togetherList;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import packman.dto.member.MemberAddDto;
 import packman.service.togetherList.TogetherListService;
 import packman.util.ResponseCode;
 import packman.util.ResponseMessage;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,11 +18,21 @@ public class TogetherListController {
     private final TogetherListService togetherListService;
 
     @GetMapping("/invite/{inviteCode}")
-    public ResponseEntity<ResponseMessage> getInviteTogetherList( @PathVariable("inviteCode") String inviteCode,HttpServletRequest request) {
+    public ResponseEntity<ResponseMessage> getInviteTogetherList(@PathVariable("inviteCode") String inviteCode, HttpServletRequest request) {
         Long userId = 1L;
         return ResponseMessage.toResponseEntity(
                 ResponseCode.SUCCESS_INVITE_TOGETHER_PACKING,
                 togetherListService.getInviteTogetherList(userId, inviteCode)
+        );
+    }
+
+    @PostMapping("/add-member")
+    public ResponseEntity<ResponseMessage> addMember(@RequestBody @Valid MemberAddDto
+                                                             memberAddDto, HttpServletRequest httpServletRequest) {
+        Long userId = 5L;
+        return ResponseMessage.toResponseEntity(
+                ResponseCode.SUCCESS_ADD_MEMBER,
+                togetherListService.addMember(memberAddDto, userId)
         );
     }
 }
