fab0e6c945bb96f954a06934603a6eeb495ced40
laalaa31 <jseoh4298@gmail.com>
2023-02-07 01:11:30 +0900

    feat: 함께 패킹리스트 담당자 배정 response code 추가 및 dto 생성

---
+++ src/main/java/packman/dto/togetherList/PackerUpdateDto.java
@@ -0,0 +1,10 @@
+package packman.dto.togetherList;
+
+import lombok.Data;
+
+@Data
+public class PackerUpdateDto {
+    String listId;
+    String packId;
+    String packerId;
+}
--- src/main/java/packman/util/ResponseCode.java
+++ src/main/java/packman/util/ResponseCode.java
@@ -62,6 +62,8 @@ public enum ResponseCode {
     SUCCESS_INVITE_TOGETHER_PACKING(HttpStatus.OK, true, "함께 패킹리스트 초대 성공"),
     SUCCESS_UPDATE_TOGETHER_CATEGORY(HttpStatus.OK, true, "함께 패킹리스트 카테고리 수정 성공"),
     SUCCESS_DELETE_TOGETHER_CATEGORY(HttpStatus.OK, true, "함께 패킹리스트 카테고리 삭제 성공"),
+    SUCCESS_UPDATE_PACKER(HttpStatus.OK, true, "함께 패킹리스트 담당자 배정 성공"),
+
 
     //togetherPackingList
     SUCCESS_CREATE_TOGETHER_LIST(HttpStatus.OK, true, "함께 패킹리스트 생성 성공"),
