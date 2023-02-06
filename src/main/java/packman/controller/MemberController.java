fbe810d9c545d62642aae1b452325f388a4312aa
dingding-21 <phj8221@naver.com>
2023-02-06 20:09:13 +0900

    feat: 함께 패킹 멤버 삭제 controller 구현

--- src/main/java/packman/controller/MemberController.java
+++ src/main/java/packman/controller/MemberController.java
@@ -3,16 +3,15 @@ package packman.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import packman.service.MemberService;
 import packman.util.ResponseCode;
 import packman.util.ResponseMessage;
+import packman.util.ResponseNonDataMessage;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.constraints.NotBlank;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,6 +27,17 @@ public class MemberController {
         return ResponseMessage.toResponseEntity(
                 ResponseCode.SUCCESS_GET_MEMBER,
                 memberService.getMember(userId, Long.valueOf(listId))
+        );
+    }
+
+    @DeleteMapping("/{groupId}/{memberId}")
+    public ResponseEntity<ResponseNonDataMessage> deleteMember(@PathVariable Long groupId, @PathVariable List<Long> memberId, HttpServletRequest request) {
+        Long userId = 1L;  // 임시 userId 1
+
+        memberService.deleteMember(userId, groupId, memberId);
+
+        return ResponseNonDataMessage.toResponseEntity(
+                ResponseCode.SUCCESS_DELETE_MEMBER
         );
     }
 }
