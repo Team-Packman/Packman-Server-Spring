7fa3710f8e819cb6a521e9b55b347050532fac93
dingding-21 <phj8221@naver.com>
2023-02-06 21:11:19 +0900

    Revert "feat: 혼자 패킹리스트 관련 validator 메서드 통합"
    This reverts commit 82415063d8fc032f4cdc5a2339938277dd662fc1.

--- src/main/java/packman/validator/Validator.java
+++ src/main/java/packman/validator/Validator.java
@@ -1,14 +1,13 @@
 package packman.validator;
 
-import packman.entity.Category;
 import packman.entity.Folder;
+import packman.repository.FolderPackingListRepository;
+import packman.repository.FolderRepository;
+import packman.entity.Category;
 import packman.entity.Pack;
 import packman.entity.UserGroup;
 import packman.entity.packingList.AlonePackingList;
 import packman.entity.packingList.PackingList;
-import packman.repository.FolderPackingListRepository;
-import packman.repository.FolderRepository;
-import packman.repository.packingList.AlonePackingListRepository;
 import packman.repository.packingList.PackingListRepository;
 import packman.repository.packingList.TogetherPackingListRepository;
 import packman.util.CustomException;
@@ -31,15 +30,10 @@ public class Validator {
         );
     }
 
-    public static PackingList validateUserAloneList(Long userId, Long aloneListId, AlonePackingListRepository alonePackingListRepository, PackingListRepository packingListRepository) {
-        PackingList packingList = validatePackingListId(packingListRepository, aloneListId);
-        AlonePackingList alonePackingList = validateAlonePackingListId(alonePackingListRepository, aloneListId);
-
+    public static void validateUserAloneList(Long userId, AlonePackingList alonePackingList) {
         if (!alonePackingList.getFolderPackingList().getFolder().getUser().getId().equals(userId)) {
             throw new CustomException(ResponseCode.NO_LIST);
         }
-
-        return packingList;
     }
 
     public static void validateListCategory(PackingList packingList, Category category) {
