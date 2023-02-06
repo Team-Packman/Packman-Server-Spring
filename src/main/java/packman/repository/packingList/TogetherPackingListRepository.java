f583afb8254c7f1b27e6d504b497fb32c6045685
dingding-21 <phj8221@naver.com>
2023-02-06 20:10:59 +0900

    feat: Group으로 함께 패킹리스트 찾는 jpa 메서드 추가

--- src/main/java/packman/repository/packingList/TogetherPackingListRepository.java
+++ src/main/java/packman/repository/packingList/TogetherPackingListRepository.java
@@ -2,6 +2,7 @@ package packman.repository.packingList;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import packman.entity.Group;
 import packman.entity.packingList.TogetherPackingList;
 
 import java.util.Optional;
@@ -9,5 +10,8 @@ import java.util.Optional;
 @Repository
 public interface TogetherPackingListRepository extends JpaRepository<TogetherPackingList, Long> {
     boolean existsByInviteCode(String inviteCode);
+
     Optional<TogetherPackingList> findByInviteCode(String inviteCode);
+
+    TogetherPackingList findByGroup(Group group);
 }
