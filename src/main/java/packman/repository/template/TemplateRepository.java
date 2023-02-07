b69a64d1a86ee78bd19cd68ac2160d0fddc1cee4
kkl4846 <kkl4846@cau.ac.kr>
2023-02-07 14:15:06 +0900

    feat: mapping으로 가져올 jpa 메소드 추가

--- src/main/java/packman/repository/template/TemplateRepository.java
+++ src/main/java/packman/repository/template/TemplateRepository.java
@@ -2,6 +2,7 @@ package packman.repository.template;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import packman.dto.template.TemplateResponseMapping;
 import packman.entity.template.Template;
 
 import java.util.Optional;
@@ -9,4 +10,5 @@ import java.util.Optional;
 @Repository
 public interface TemplateRepository extends JpaRepository<Template, Long> {
     Optional<Template> findByIdAndIsDeleted(Long templateId, boolean isDeleted);
+    TemplateResponseMapping findByIdAndTitle(Long templateId, String title);
 }
