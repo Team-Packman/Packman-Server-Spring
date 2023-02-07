d222650d2102ad0fd8c828adc0bbdaf0bb1a5b05
kkl4846 <kkl4846@cau.ac.kr>
2023-02-07 14:14:15 +0900

    feat: validator 추가

--- src/main/java/packman/validator/Validator.java
+++ src/main/java/packman/validator/Validator.java
@@ -1,11 +1,9 @@
 package packman.validator;
 
-import packman.entity.Folder;
+import packman.entity.*;
+import packman.entity.template.Template;
 import packman.repository.FolderPackingListRepository;
 import packman.repository.FolderRepository;
-import packman.entity.Category;
-import packman.entity.Pack;
-import packman.entity.UserGroup;
 import packman.entity.packingList.AlonePackingList;
 import packman.entity.packingList.PackingList;
 import packman.repository.packingList.PackingListRepository;
@@ -59,5 +57,11 @@ public class Validator {
         validateUserMemberId(userGroups, userId);
 
         return packingList;
+    }
+
+    public static void validateUserTemplate(Template template, User user) {
+        if (template.getUser() != null && template.getUser() != user) {
+            throw new CustomException(ResponseCode.NO_TEMPLATE);
+        }
     }
 }
