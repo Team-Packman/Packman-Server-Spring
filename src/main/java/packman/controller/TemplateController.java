e075309abbc563889c5b4183121843b287fbfb3b
kkl4846 <kkl4846@cau.ac.kr>
2023-02-07 14:15:25 +0900

    feat: 템플릿 상세조회 컨트롤러 추가

--- src/main/java/packman/controller/TemplateController.java
+++ src/main/java/packman/controller/TemplateController.java
@@ -1,11 +1,29 @@
 package packman.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import packman.service.TemplateService;
+import packman.util.ResponseCode;
+import packman.util.ResponseMessage;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/template")
 public class TemplateController {
+    private final TemplateService templateService;
+
+    @GetMapping("/{templateId}")
+    public ResponseEntity<ResponseMessage> getTemplate(@PathVariable Long templateId, HttpServletRequest request) {
+        Long userId = 1L;
+        return ResponseMessage.toResponseEntity(
+                ResponseCode.SUCCESS_GET_DETAILED_TEMPLATE,
+                templateService.getTemplate(templateId, userId)
+        );
+    }
 }
