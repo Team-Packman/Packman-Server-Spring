0e1e29c2d10cd9778145078769025c77fc5c4729
kkl4846 <kkl4846@cau.ac.kr>
2023-02-07 14:15:43 +0900

    feat: 템플릿 상세조회 서비스 구현

--- src/main/java/packman/service/TemplateService.java
+++ src/main/java/packman/service/TemplateService.java
@@ -3,9 +3,35 @@ package packman.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import packman.dto.template.TemplateResponseDto;
+import packman.dto.template.TemplateResponseMapping;
+import packman.entity.User;
+import packman.entity.template.Template;
+import packman.repository.UserRepository;
+import packman.repository.template.TemplateRepository;
+
+import static packman.validator.IdValidator.validateTemplateId;
+import static packman.validator.IdValidator.validateUserId;
+import static packman.validator.Validator.validateUserTemplate;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class TemplateService {
+    private final TemplateRepository templateRepository;
+    private final UserRepository userRepository;
+
+    public TemplateResponseDto getTemplate(Long templateId, Long userId) {
+        User user = validateUserId(userRepository, userId);
+        Template template = validateTemplateId(templateRepository, templateId);
+
+        validateUserTemplate(template, user);
+
+        TemplateResponseMapping templateResponseMapping = templateRepository.findByIdAndTitle(template.getId(), template.getTitle());
+
+        return TemplateResponseDto.builder()
+                .id(templateResponseMapping.getId())
+                .title(templateResponseMapping.getTitle())
+                .category(templateResponseMapping.getCategories()).build();
+    }
 }
