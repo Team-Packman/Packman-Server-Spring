43af3f5d0310418c104b40832b49be6ab84e89af
dingding-21 <phj8221@naver.com>
2023-02-12 15:19:22 +0900

    feat: code로 카카오에서 user의 카카오 accessToken을 받아오는 service 메서드 구현

---
+++ src/main/java/packman/dto/auth/KakaoInfoResponseDto.java
@@ -0,0 +1,13 @@
+package packman.dto.auth;
+
+import lombok.Data;
+
+@Data
+public class AuthKakaoResponseDto {
+    private String access_token;
+    private String token_type;
+    private String refresh_token;
+    private String expires_in;
+    private String scope;
+    private int refresh_token_expires_in;
+}
--- src/main/java/packman/service/AuthService.java
+++ src/main/java/packman/service/AuthService.java
@@ -1,11 +1,78 @@
 package packman.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import packman.auth.JwtTokenProvider;
+import packman.dto.auth.AuthKakaoResponseDto;
+import packman.repository.UserRepository;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class AuthService {
+    private final UserRepository userRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+    @Value("${spring.security.oauth2.client.registration.kakao.client-id}")
+    private String kakaoClientId;
+    @Value("${spring.security.oauth2.client.registration.kakao.redirect-uri}")
+    private String kakaoRedirectUri;
+    @Value("${spring.security.oauth2.client.registration.kakao.client-secret}")
+    private String kakaoClientSecret;
+
+    @Value("${kakao.authUrl}")
+    private String kakaoAuthUrl;
+
+    @Value("${kakao.userProfileUrl}")
+    private String kakaoUserProfileUrl;
+    
+
+    // 서버에서 code로 accessToken 생성할 때 사용
+    public String getKakaoAccessToken(String code) {
+        RestTemplate rt = new RestTemplate();
+
+        // HttpHeader 오브젝트 생성
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        // HttpBody 오브젝트 생성
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", kakaoClientId);
+        params.add("redirect_uri", kakaoRedirectUri);
+        params.add("code", code);
+        params.add("client_secret", kakaoClientSecret);
+
+        // HttpHeader와 HttpsBody를 하나의 오브젝트에 담기
+        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(params, headers);
+
+        // Http 요청하기
+        ResponseEntity<String> response = rt.exchange(
+                kakaoAuthUrl,
+                HttpMethod.POST,
+                kakaoTokenRequest,
+                String.class
+        );
+
+        // Gson, Json Simple, ObjectMapper
+        ObjectMapper objectMapper = new ObjectMapper();
+        AuthKakaoResponseDto authKakaoResponseDto;
+        try {
+            authKakaoResponseDto = objectMapper.readValue(response.getBody(), AuthKakaoResponseDto.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        return authKakaoResponseDto.getAccess_token();
+    }
 }
