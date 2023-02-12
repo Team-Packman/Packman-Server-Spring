35c14e460efe1768c5b5d7c236775ec70da3c364
dingding-21 <phj8221@naver.com>
2023-02-12 14:59:59 +0900

    feat: 카카오 로그인 controller 구현

--- src/main/java/packman/controller/AuthController.java
+++ src/main/java/packman/controller/AuthController.java
@@ -1,11 +1,52 @@
 package packman.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import packman.auth.JwtTokenProvider;
+import packman.dto.auth.AuthKakaoLoginNewResponseDto;
+import packman.dto.auth.AuthKakaoLoginResponseDto;
+import packman.dto.auth.AuthKakaoTokenDto;
+import packman.service.AuthService;
+import packman.util.ResponseCode;
+import packman.util.ResponseMessage;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
+    private final AuthService authService;
+    private final JwtTokenProvider jwtTokenProvider;
+
+
+    @PostMapping("/kakao")
+    public ResponseEntity<ResponseMessage> kakaoLogin(@RequestBody @Valid AuthKakaoTokenDto authKakaoTokenDto) {
+
+        AuthKakaoLoginResponseDto kakaoUserProfile = authService.getKakaoUserProfile(authKakaoTokenDto);
+
+        if (!kakaoUserProfile.isAlreadyUser()) {
+            AuthKakaoLoginNewResponseDto authKakaoLoginNewResponseDto = AuthKakaoLoginNewResponseDto.builder()
+                    .alreadyUser(kakaoUserProfile.isAlreadyUser())
+                    .email(kakaoUserProfile.getEmail())
+                    .name(kakaoUserProfile.getName())
+                    .gender(kakaoUserProfile.getGender())
+                    .ageRange(kakaoUserProfile.getAgeRange())
+                    .build();
+
+            return ResponseMessage.toResponseEntity(
+                    ResponseCode.SUCCESS_KAKAO_LOGIN,
+                    authKakaoLoginNewResponseDto
+            );
+        }
+
+        return ResponseMessage.toResponseEntity(
+                ResponseCode.SUCCESS_KAKAO_LOGIN,
+                kakaoUserProfile
+        );
+    }
 }
