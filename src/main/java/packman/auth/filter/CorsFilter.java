59d36b6fb8cfd9a6ba6a3500a1c3371fd2ad3ecc
dingding-21 <phj8221@naver.com>
2023-04-10 18:01:58 +0900

    chore: cors 허용 url 수정

--- src/main/java/packman/auth/filter/CorsFilter.java
+++ src/main/java/packman/auth/filter/CorsFilter.java
@@ -17,7 +17,7 @@ public class CorsFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
 
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.setHeader("Access-Control-Allow-Origin", "www.packman.kr");
 //        response.setHeader("Access-Control-Allow-Origin", "*");
 
         response.setHeader("Access-Control-Allow-Credentials", "true");
