9ec6f64e80fd1215d761fb20745997f244438a27
dingding-21 <phj8221@naver.com>
2023-04-13 14:38:39 +0900

    feat: 각종 exception 로그

--- src/main/java/packman/util/ExceptionHandler.java
+++ src/main/java/packman/util/ExceptionHandler.java
@@ -1,5 +1,7 @@
 package packman.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -9,14 +11,17 @@ import javax.validation.ConstraintViolationException;
 
 @ControllerAdvice
 public class ExceptionHandler {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @org.springframework.web.bind.annotation.ExceptionHandler(value = Exception.class)
     protected ResponseEntity<ResponseNonDataMessage> handleCustomException(Exception e) {
         if (e instanceof CustomException) {
+            logger.error(((CustomException) e).getResponseCode().getMessage());
             return ResponseNonDataMessage.toResponseEntity(((CustomException) e).getResponseCode());
         } else if (e instanceof ConstraintViolationException || e instanceof MethodArgumentNotValidException || e instanceof HttpMessageNotReadableException) {
             return ResponseNonDataMessage.toResponseEntity(new CustomException(ResponseCode.NULL_VALUE).getResponseCode());
         }
-        System.out.println(e);
+        logger.error(e.getMessage());
         return ResponseNonDataMessage.toResponseEntity(new CustomException(ResponseCode.BAD_REQUEST).getResponseCode());
     }
 }
