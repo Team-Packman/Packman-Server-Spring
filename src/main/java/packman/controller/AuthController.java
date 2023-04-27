5adac313dcc59d55882a2ffd5790df52d1175a41
dingding-21 <phj8221@naver.com>
2023-04-27 15:03:14 +0900

    chore: 토큰 재발급 cors 허용 url 수정

--- src/main/java/packman/controller/AuthController.java
+++ src/main/java/packman/controller/AuthController.java
@@ -47,7 +47,7 @@ public class AuthController {
         );
     }
 
-    @CrossOrigin(origins = {"https://www.packman.kr", "https://packgirl.ml", "http://localhost:3000"}, allowedHeaders = "*")
+    @CrossOrigin(origins = {"https://www.packman.kr", "https://www.packgirl.ml", "http://localhost:3000"}, allowedHeaders = "*")
     @GetMapping("/token")
     public ResponseEntity<ResponseMessage> getNewToken(HttpServletRequest request) {
         String accessToken = jwtTokenProvider.resolveAccessToken(request);
